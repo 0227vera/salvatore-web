400d8d581491601116cec0adf4e67d7b33cb6e8a
廖轩 <liaoxuan@lezhixing.com.cn>
2020-01-10 17:39:56 +0800

    feat: set

--- utils/produ-id.js
+++ utils/produ-id.js
@@ -7,9 +7,8 @@ let  randomWord = (randomFlag, min, max) => {
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
         'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
         'U', 'V', 'W', 'X', 'Y', 'Z','a','b','c','d','e',
-        '!','@','#','$','%','^','&','*','f','g','h','i',
-        'j','k','l','m','n','o','p','q','r','s','t','u',
-        'v','w','x','y','z'];
+        'f','g','h','i','j','k','l','m','n','o','p','q',
+        'r','s','t','u','v','w','x','y','z'];
   // 随机产生
   if (randomFlag) {
       range = Math.round(Math.random() * (max - min)) + min;
@@ -28,3 +27,12 @@ module.exports =  () => {
   var hashcode = myRandom.slice(0,8)+ timestamp + myRandom.slice(-8)
   return hashcode;
 }
+
+let test = () => {
+  //定义一个时间戳，用来获得唯一时间 可以保证hash码是唯一的
+  var timestamp = (new Date()).valueOf();
+  var myRandom = randomWord(true, 16, 24);
+  var hashcode = myRandom.slice(0,8)+ timestamp + myRandom.slice(-8)
+  return hashcode;
+}
+console.log(test())
